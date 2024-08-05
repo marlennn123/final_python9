d0f0a1c162af89cf6640467f35c2a22f42317c23
Bayamanbayh <guglovdiskord@gmail.com>
2024-08-05 09:42:20 +0600

    ya bistro

---
+++ Bayaman/calculator.py
@@ -0,0 +1,8 @@
+number = int(input('write a number: '))
+
+print('Start')
+while number != -1:
+    print(number)
+    number -= 1
+
+print('The end')
---
+++ Bayaman/debilizm.py
@@ -0,0 +1,7 @@
+number = int(input('write a number: '))
+
+print('Start')
+for i in range(number, -1, -1):
+    print(i)
+
+print('The end')
